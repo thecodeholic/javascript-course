9ab23ace26f02f50eabd27303e22097fbb9863d2
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-09-01 23:31:41 +0400

    Write comments for lesson 1

---
+++ app.js
@@ -0,0 +1,21 @@
+// alert, confirm, prompt
+
+// console
+
+// Accessing DOM
+
+// Writing in DOM. document.write
+
+// variables, types. let vs const vs var
+
+// comments
+
+// strings/string methods
+
+// conditionals, ternary
+
+// loops (while, do-while, for)
+
+// functions
+
+// Write program to calculate n factorial
